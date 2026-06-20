85f536c2c285948fabf11c79210c249dfde035cf
FameNu <110712106+FameNu@users.noreply.github.com>
2026-06-20 14:37:49 +0700

    feat: add SystemColor type and export in index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,3 @@
 export * from './tubes'
 export * from './solver'
+export * from './systemColors'
---
+++ src/types/systemColors.ts
@@ -0,0 +1,9 @@
+export type SystemColor = {
+  id: string
+  name: string
+  hex: string
+  insideColor: {
+    name: string
+    hex: string
+  }
+}
