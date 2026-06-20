fbc81305045b39a319883e050209dd391eb0eef6
FameNu <110712106+FameNu@users.noreply.github.com>
2026-06-20 14:37:59 +0700

    feat: update SYSTEM_COLORS with detailed color information and hex values

--- src/utils/systemColors.ts
+++ src/utils/systemColors.ts
@@ -1,14 +1,76 @@
-export const SYSTEM_COLORS = [
-  { id: 'c1', name: 'Red', hex: '#e53935' },
-  { id: 'c2', name: 'Dark Red', hex: '#8b0000' },
-  { id: 'c3', name: 'Light Green', hex: '#81c784' },
-  { id: 'c4', name: 'Dark Green', hex: '#2e7d32' },
-  { id: 'c5', name: 'Blue', hex: '#1e88e5' },
-  { id: 'c6', name: 'Light Blue', hex: '#81d4fa' },
-  { id: 'c7', name: 'Purple', hex: '#8e24aa' },
-  { id: 'c8', name: 'Light Purple', hex: '#ce93d8' },
-  { id: 'c9', name: 'Orange', hex: '#ff9800' },
-  { id: 'c10', name: 'Pink', hex: '#f06292' },
-  { id: 'c11', name: 'Beige', hex: '#e3c298' },
-  { id: 'c12', name: 'White', hex: '#ffffff' },
+import type { SystemColor } from '@types'
+
+export const SYSTEM_COLORS: SystemColor[] = [
+  {
+    id: 'c1',
+    name: 'Red',
+    hex: '#FE5658',
+    insideColor: { name: 'Inner Red', hex: '#D13F40' },
+  },
+  {
+    id: 'c2',
+    name: 'Dark Red',
+    hex: '#872E2B',
+    insideColor: { name: 'Inner Dark Red', hex: '#691918' },
+  },
+  {
+    id: 'c3',
+    name: 'Light Green',
+    hex: '#4BCD49',
+    insideColor: { name: 'Inner Light Green', hex: '#20A41D' },
+  },
+  {
+    id: 'c4',
+    name: 'Dark Green',
+    hex: '#719883',
+    insideColor: { name: 'Inner Dark Green', hex: '#295546' },
+  },
+  {
+    id: 'c5',
+    name: 'Blue',
+    hex: '#3673E2',
+    insideColor: { name: 'Inner Blue', hex: '#1B56CA' },
+  },
+  {
+    id: 'c6',
+    name: 'Light Blue',
+    hex: '#1DB4D3',
+    insideColor: { name: 'Inner Light Blue', hex: '#048BA9' },
+  },
+  {
+    id: 'c7',
+    name: 'Purple',
+    hex: '#7C2CB1',
+    insideColor: { name: 'Inner Purple', hex: '#A23DE5' },
+  },
+  {
+    id: 'c8',
+    name: 'Light Purple',
+    hex: '#B9ABE1',
+    insideColor: { name: 'Inner Light Purple', hex: '#8070AF' },
+  },
+  {
+    id: 'c9',
+    name: 'Orange',
+    hex: '#F9992B',
+    insideColor: { name: 'Inner Orange', hex: '#C06D0F' },
+  },
+  {
+    id: 'c10',
+    name: 'Pink',
+    hex: '#FFA2A3',
+    insideColor: { name: 'Inner Pink', hex: '#E66B6D' },
+  },
+  {
+    id: 'c11',
+    name: 'Beige',
+    hex: '#E0BB8F',
+    insideColor: { name: 'Inner Beige', hex: '#C08F55' },
+  },
+  {
+    id: 'c12',
+    name: 'White',
+    hex: '#E7F1F3',
+    insideColor: { name: 'Inner White', hex: '#CAD2DF' },
+  },
 ]
